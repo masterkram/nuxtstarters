4011e3a4ba2eaf7625b498fbc26118e2695c3acf
Mark Bruderer <markbruderer123@gmail.com>
2024-09-19 12:38:36 +0200

    fix: remove og image.

--- pages/[...all].vue
+++ pages/[...all].vue
@@ -9,18 +9,6 @@ if (!(page as any).value && process.server) {
   event.res.statusCode = 404
 }
 
-useSeoMeta({
-  title: () => page.value?.title,
-  description: () => page.value?.description,
-});
-
-defineOgImageComponent('NuxtSeo', {
-  title: 'Hello OG Image 👋',
-  description: 'Look what at me in dark mode',
-  theme: '#ff0000',
-  colorMode: 'dark',
-});
-
 definePageMeta({ middleware: ['search-from-url'] })
 </script>
 
