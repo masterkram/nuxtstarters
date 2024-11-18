a75d58f6e466d5dace013ab860c656116b36172e
Mark Bruderer <markbruderer123@gmail.com>
2024-11-18 19:39:14 +0100

    fix: styling updates.

--- components/content/hero/Thin.vue
+++ components/content/hero/Thin.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="tracking-tight dashcue">
+    <h1 class="tracking-tight text-5xl">
       <ContentSlot :use="$slots.default" unwrap="h1" />
     </h1>
     <p class="max-w-lg text-md leading-8">
