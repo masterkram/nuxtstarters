f0d0e519651ec3e32c63d7b06572a85e9ec98516
Mark Bruderer <markbruderer123@gmail.com>
2025-03-03 21:06:07 +0100

    fix: posthog setup

--- plugins/posthog.client.js
+++ plugins/posthog.client.js
@@ -1,13 +1,9 @@
 import { defineNuxtPlugin } from '#app'
 import posthog from 'posthog-js'
-
-export default defineNuxtPlugin({
-  name: "posthog",
-  parallel: true,
-}, nuxtApp => {
+export default defineNuxtPlugin(nuxtApp => {
   const runtimeConfig = useRuntimeConfig();
   const posthogClient = posthog.init(runtimeConfig.public.posthogPublicKey, {
-    api_host: runtimeConfig.public.posthogHost,
+    api_host: runtimeConfig.public.posthogHost || 'https://us.i.posthog.com',
     capture_pageview: false, // we add manual pageview capturing below
     loaded: (posthog) => {
       if (import.meta.env.MODE === 'development') posthog.debug();
